e29227744cb79bd98cbc7f396dbea71bf9c11ac5
torakichi <tor4kichi@gmail.com>
2016-01-23 09:29:37 +0900

    FiltersのFileとFolderを実装。

--- ReactiveFolderModel/Filters/FileReactiveFilter.cs
+++ ReactiveFolderModel/Filters/FileReactiveFilter.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
 
 namespace ReactiveFolder.Model.Filters
 {
+	[DataContract]
 	public class FileReactiveFilter : ReactionFilterBase
 	{
+		[DataMember]
+		public string FileFilterPattern { get; set; }
+
+		public FileReactiveFilter()
+		{
+			FileFilterPattern = "*.*";
+		}
+
 		// TODO: ファイル名のフィルタ実装
 		protected override IEnumerable<FileInfo> FileFilter(DirectoryInfo workDir)
 		{
-			return workDir.EnumerateFiles();
+			return workDir.EnumerateFiles(FileFilterPattern, SearchOption.TopDirectoryOnly);
 		}
 	}
 }
--- ReactiveFolderModel/Filters/FolderReactiveFilter.cs
+++ ReactiveFolderModel/Filters/FolderReactiveFilter.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
 
 namespace ReactiveFolder.Model.Filters
 {
+	[DataContract]
 	public class FolderReactiveFilter : ReactionFilterBase
 	{
+		[DataMember]
+		public string FolderFilterPattern { get; set; }
+
+
+		public FolderReactiveFilter()
+		{
+			FolderFilterPattern = "*";
+		}
+
+
 		protected override IEnumerable<DirectoryInfo> DirectoryFilter(DirectoryInfo workDir)
 		{
-			return workDir.EnumerateDirectories();
+			return workDir.EnumerateDirectories(FolderFilterPattern, SearchOption.TopDirectoryOnly);
 		}
 	}
 }
