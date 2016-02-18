7394bd398d3f7f8f8fb9df40105ebbf741692abd
torakichi <tor4kichi@gmail.com>
2016-02-18 18:27:13 +0900

    #9  ショートカットの作成と削除をサポート

---
+++ ReactiveFolder/ShortcutInstallCustomAction.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using ReactiveFolder.Model.Util;
+using System.IO;
+using System.Diagnostics;
+
+namespace ReactiveFolder
+{
+	[RunInstaller(true)]
+	public partial class ShortcutInstallCustomAction : System.Configuration.Install.Installer
+	{
+		public const string APP_NAME = @"ReactiveFolder.exe";
+		public const string LINK_NAME = @"Launch ReactiveFolder";
+
+
+		public ShortcutInstallCustomAction()
+		{
+			InitializeComponent();
+		}
+
+		public override void Install(IDictionary stateSaver)
+		{
+			base.Install(stateSaver);
+
+		}
+
+		protected override void OnAfterInstall(IDictionary savedState)
+		{
+			if (Context.Parameters["DesktopShortcut"] == "1")
+			{
+				CreateApplicationShortcut(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+			}
+
+			if (Context.Parameters["AddToStartup"] == "1")
+			{
+				CreateApplicationShortcut(Environment.GetFolderPath(Environment.SpecialFolder.Startup));
+			}
+		}
+
+
+		private void CreateApplicationShortcut(string targetFolder)
+		{
+			var appDir = Context.Parameters["dir"];
+			var appPath = Path.Combine(appDir, APP_NAME);
+			ShortcutHelper.CreateShortcut(appPath, targetFolder, LINK_NAME);
+		}
+
+
+
+
+
+
+
+
+		public override void Rollback(IDictionary savedState)
+		{
+			base.Rollback(savedState);
+
+			DeleteApplicationShortcurt(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+			DeleteApplicationShortcurt(Environment.GetFolderPath(Environment.SpecialFolder.Startup));
+		}
+
+		public override void Uninstall(IDictionary savedState)
+		{
+			base.Uninstall(savedState);
+
+			DeleteApplicationShortcurt(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+			DeleteApplicationShortcurt(Environment.GetFolderPath(Environment.SpecialFolder.Startup));
+		}
+
+		
+
+		private void DeleteApplicationShortcurt(string targetFolder)
+		{
+			var linkPath = ShortcutHelper.MakeShortcutPath(targetFolder, LINK_NAME);
+
+			var fileInfo = new FileInfo(linkPath);
+			if(fileInfo.Exists)
+			{
+				fileInfo.Delete();
+			}
+
+		}
+	}
+}
---
+++ ReactiveFolderModel/Util/ShortcutHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using IWshRuntimeLibrary;
+
+
+namespace ReactiveFolder.Model.Util
+{
+	public static class ShortcutHelper
+	{
+		
+
+		public static void CreateShortcut(string sourcePath, string targetFolder, string linkName, string iconLocation = null)
+		{
+			//作成するショートカットのパス
+			string shortcutPath = MakeShortcutPath(targetFolder, linkName);
+
+			//WshShellを作成
+			Type t = Type.GetTypeFromCLSID(new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8"));
+			IWshShell shell = Activator.CreateInstance(t) as IWshShell;
+			//WshShortcutを作成
+			var shortcut = shell.CreateShortcut(shortcutPath) as IWshShortcut;
+
+
+			//リンク先
+			shortcut.TargetPath = sourcePath;
+
+			//アイコンのパス
+			if (iconLocation != null)
+			{
+				shortcut.IconLocation = iconLocation;
+			}
+			//その他のプロパティも同様に設定できるため、省略
+			
+			//ショートカットを作成
+			shortcut.Save();
+
+			//後始末
+			System.Runtime.InteropServices.Marshal.FinalReleaseComObject(shortcut);
+			System.Runtime.InteropServices.Marshal.FinalReleaseComObject(shell);
+		}	
+
+		public static string MakeShortcutPath(string targetFolder, string linkName)
+		{
+			return System.IO.Path.Combine(
+			   targetFolder,
+			   $"{linkName}.lnk");
+		}
+	}
+}
